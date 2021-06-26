35bf9d69150b78c4bdd66f33fe0a98c44bbced45
ovodima <dima.tsymbal12345@gmail.com>
2021-06-26 14:06:54 +0300

    add style

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -4,22 +4,25 @@ import productData from '../data/Data'
 
 const Content = () => {
     const product = productData.map((item) => 
-    <div className = 'card' key={item.id}>
-        <div className = 'card_img'>
-            <img src = {item.img}/>
-        </div>
+    
+        <div className = 'card' key={item.id}>
+            <div className = 'card_img'>
+                <img src = {item.img}/>
+            </div>
 
-        <div className = 'card_header'>
-            <h2>{item.name}</h2>
-            <p>{item.description}</p>
-            <p className='price'>{item.price + ' ' + item.currency}</p>
-        </div>
-    </div>)
-
-    return (
+            <div className = 'card_header'>
+                <p>{item.position}</p>
+                <h2>{item.name}</h2>
+                <p className='price'>{item.price + ' ' + item.currency}</p>
+            </div>
+        </div>)
+        
+        return (
         <div>
             <h1 className = 'heading'>Наша Продукция</h1>
-            {product}
+            <div className = 'grid-container'>
+                {product}
+            </div>
         </div>
     )
 }
--- src/data/Data.js
+++ src/data/Data.js
@@ -3,130 +3,143 @@ const productData = [
         id: 1,
         name: '4G180',
         description: 'Tenda 4G LTE 150Mbps Mobile WiFi Router | 4G180',
+        position: 'Роутер',
         stock: '106',
         price: '952.17',
         currency: 'грн.',
-        img: 'https://scoop.co.za/download/marketing/images/4G180.jpg'
+        img: 'https://scoop.co.za/download/marketing/images/4G180.jpg',
     },
 
     {
         id: 2,
         name: '4G680',
         description: 'Tenda 4G LTE 300Mbps WiFi Router | 4G680',
+        position: 'Роутер',
         stock: '0',
         price: '1252.17',
         currency: 'грн.',
-        img: 'https://scoop.co.za/download/marketing/images/4G680.jpg'
+        img: 'https://scoop.co.za/download/marketing/images/4G680.jpg',
     },
 
     {
         id: 3,
         name: 'ACB-AC',
         description: 'Ubiquiti AirCube AC WiFi Router | ACB-AC',
+        position: 'Роутер',
         stock: '124',
         price: '1282.61',
         currency: 'грн.',
-        img: 'https://scoop.co.za/download/marketing/images/ACB-AC.jpg'
+        img: 'https://scoop.co.za/download/marketing/images/ACB-AC.jpg',
     },
 
     {
         id: 4,
         name: 'ACB-ISP',
         description: 'Ubiquiti AirCube WiFi PoE Access Point with UNMS | ACB-ISP',
+        position: 'Роутер',
         stock: '296',
         price: '478.26',
         currency: 'грн.',
-        img: 'https://scoop.co.za/download/marketing/images/ACB-ISP.jpg'
+        img: 'https://scoop.co.za/download/marketing/images/ACB-ISP.jpg',
     },
 
     {
         id: 5,
         name: 'AF-11FX',
         description: 'Ubiquiti AirFiberX 11GHz PtP Radio | AF-11FX',
+        position: 'Роутер',
         stock: '55',
         price: '10691.3',
         currency: 'грн.',
-        img: 'https://scoop.co.za/download/marketing/images/AF-11FX.jpg'
+        img: 'https://scoop.co.za/download/marketing/images/AF-11FX.jpg',
     },
 
     {
         id: 6,
         name: 'AF-11FXDUPL',
         description: 'Ubiquiti AirFiberX 11Ghz Low Band Duplexer | AF-11FX-DUP-L',
+        position: 'Роутер',
         stock: '102',
         price: '3300',
         currency: 'грн.',
-        img: 'https://scoop.co.za/download/marketing/images/AF-11FXDUPL.jpg'
+        img: 'https://scoop.co.za/download/marketing/images/AF-11FXDUPL.jpg',
     },
 
     {
         id: 7,
         name: 'AF-11G35',
         description: 'Ubiquiti AirFiber Dish 11Ghz 35dBi 81cm | AF-11G35',
+        position: 'Роутер',
         stock: '74',
         price: '6652.17',
         currency: 'грн.',
-        img: 'https://scoop.co.za/download/marketing/images/AF-11G35.jpg'
+        img: 'https://scoop.co.za/download/marketing/images/AF-11G35.jpg',
     },
 
     {
         id: 8,
         name: 'AF-5XHD',
         description: 'Ubiquiti AirFiber5X HD PtP Radio Gigabit WiFi | AF-5XHD',
+        position: 'Роутер',
         stock: '190',
         price: '6865.22',
         currency: 'грн.',
-        img: 'https://scoop.co.za/download/marketing/images/AF-5XHD.jpg'
+        img: 'https://scoop.co.za/download/marketing/images/AF-5XHD.jpg',
     },
 
     {
         id: 9,
         name: 'AFI-INSK',
         description: 'Ubiquiti AmpliFi Instant Router +1 MeshPoint Kit | AFI-INS',
+        position: 'Роутер',
         stock: '155',
         price: '3126.09',
         currency: 'грн.',
-        img: 'https://scoop.co.za/download/marketing/images/AFI-INSK.jpg'
+        img: 'https://scoop.co.za/download/marketing/images/AFI-INSK.jpg',
     },
 
     {
         id: 10,
         name: 'AFI-INSR',
         description: 'Ubiquiti AmpliFi Instant Router | AFI-INS-R',
+        position: 'Роутер',
         stock: '78',
         price: '1717.39',
         currency: 'грн.',
-        img: 'https://scoop.co.za/download/marketing/images/AFI-INSR.jpg'
+        img: 'https://scoop.co.za/download/marketing/images/AFI-INSR.jpg',
     },
 
     {
         id: 11,
         name: 'AIR-D523X',
         description: 'Ubiquiti 5GHz AirFiber Dish 23dBi Slant 45 PtP | AF-5G23-S45',
+        position: 'Роутер',
         stock: '57',
         price: '1821.74',
         currency: 'грн.',
-        img: 'https://scoop.co.za/download/marketing/images/AIR-D523X.jpg'
+        img: 'https://scoop.co.za/download/marketing/images/AIR-D523X.jpg',
     },
 
     {
         id: 12,
         name: 'AIR-D530',
         description: 'Ubiquiti 5GHz AirMax Dish 30dBi Backhaul PtP | RD-5G30',
+        position: 'Роутер',
         stock: '40',
         price: '2304.35',
         currency: 'грн.',
-        img: 'https://scoop.co.za/download/marketing/images/AIR-D530.jpg'
+        img: 'https://scoop.co.za/download/marketing/images/AIR-D530.jpg',
     },
 
    {
         id: 13,
         name: 'AIR-D530LW',
         description: 'Ubiquiti 5GHz AirMax Dish 30dBi Light Weight PtP | RD-5G30-LW',
+        position: 'Роутер',
         stock: '16',
         price: '1734.78',
         currency: 'грн.',
-        img: 'https://scoop.co.za/download/marketing/images/AIR-D530LW.jpg'
+        img: 'https://scoop.co.za/download/marketing/images/AIR-D530LW.jpg',
     },
 
     
