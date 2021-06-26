5cb44657aad3ac7afe366d749c8d284de5d703b3
ovodima <dima.tsymbal12345@gmail.com>
2021-06-26 10:54:39 +0300

    create components

---
+++ src/data/Data.js
@@ -0,0 +1,11 @@
+const productData = [
+    {
+        name: '4G180',
+        description: 'Tenda 4G LTE 150Mbps Mobile WiFi Router | 4G180',
+        stock: '106',
+        price: '952.17',
+        currency: 'грн.',
+        img: 'https://scoop.co.za/download/marketing/images/4G180.jpg'
+
+    }
+]
